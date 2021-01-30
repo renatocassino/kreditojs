89d960f1343e4778c079b77061dbe0ab3119f889
Cassino <renatocassino@gmail.com>
2021-01-30 03:09:53 -0300

    Rename the lib

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import Kreditoj from './Kreditoj.vue';
 import './registerServiceWorker';
 
-createApp(App).mount('#app');
+createApp(Kreditoj).mount('#app');
